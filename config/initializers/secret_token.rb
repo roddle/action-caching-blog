e65f146eb0ec49da27828f38887d6a874dec1f21
Todd Anderson <todd@thehomie.com>
2011-07-08 15:28:35 -0700

    blog without caching on sqlite3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '3bfe300c2606c31e60ef125d35364be5fe15ee02a6d4af1d848f9955209cde997ed0663d9f54427db89c6e4bc98434097038a93a7b2fd519deb9707ee74f05ae'
